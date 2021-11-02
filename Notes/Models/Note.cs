59e978e1891ba81be1c0293f18a17fd5fcc74585
Senates <coopsnoopftw@gmail.com>
2021-11-02 19:37:40 +1300

    commit

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SQLite;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
